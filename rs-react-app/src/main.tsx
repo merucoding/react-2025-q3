1fda43c888fe2afc0d4b328f141e0ca2c29557bf
Meruert <mika.amantaikuzu@gmail.com>
2025-07-13 19:23:54 +0500

    feat: add Header component

--- rs-react-app/src/main.tsx
+++ rs-react-app/src/main.tsx
@@ -1,12 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import App from './App.tsx';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Root element not found');
 
-createRoot(rootElement!).render(
+createRoot(rootElement).render(
   <StrictMode>
     <App />
   </StrictMode>
